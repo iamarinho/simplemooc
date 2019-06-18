90a73d6efe0c92dfc3882e1f1d4543f36d4fe7b5
Iasmy <iasmyiasmy@gmail.com>
2019-06-18 17:00:55 -0300

    ajustes

--- simplemooc/urls.py
+++ simplemooc/urls.py
@@ -21,5 +21,5 @@ urlpatterns += patterns('django.views.static',
     url(pattern, 'serve', {'document_root': settings.STATIC_ROOT}),
 )
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#if settings.DEBUG:
+#    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
