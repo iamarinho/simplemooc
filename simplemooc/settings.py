60af8745bf35669f883849f94e0fe08c66ea2a00
iamarinho <iasmyiasmy@gmail.com>
2019-06-18 15:28:24 -0300

    Update settings.py

--- simplemooc/settings.py
+++ simplemooc/settings.py
@@ -135,7 +135,7 @@ DEFAULT_FROM_EMAIL = 'Nome <iaiapeliculas@gmail.com>'
 EMAIL_USE_TLS = True
 #EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'iaiapeliculas@gmail.com'
-#EMAIL_HOST_PASSWORD = 'explain@456'
+#EMAIL_HOST_PASSWORD = ''
 #EMAIL_PORT = 587
 
 CONTACT_EMAIL = 'contato@simplemooc.com'
