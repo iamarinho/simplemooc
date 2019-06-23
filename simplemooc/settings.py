ff485af686e5e8b15f64958c5e60b4d7421ae9cb
Iasmy <iasmyiasmy@gmail.com>
2019-06-23 16:59:40 -0300

    ajustes

--- simplemooc/settings.py
+++ simplemooc/settings.py
@@ -133,9 +133,9 @@ MEDIA_URL = '/media/'
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-#STATICFILES_DIRS = (
-#    os.path.join(BASE_DIR, 'static'),
-#   )
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 #STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
