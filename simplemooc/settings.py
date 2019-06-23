d7d7f33eb724bd0e6145ec0b067fadca6d21e0b2
Iasmy <iasmyiasmy@gmail.com>
2019-06-23 17:01:20 -0300

    ajustes

--- simplemooc/settings.py
+++ simplemooc/settings.py
@@ -131,7 +131,7 @@ MEDIA_URL = '/media/'
 #STATIC_ROOT = 'staticfiles'
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
